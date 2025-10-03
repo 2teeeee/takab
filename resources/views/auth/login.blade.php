254da6766a20c9a23e7e6803cc5c661965343813
ahmad reza jalali <hamed.classic.2te@gmail.com>
2025-10-03 19:09:57 +0330

    login & register form ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,30 @@
 <x-main-layout>
-    <div class="row justify-content-center my-5 py-5">
-        <div class="col-md-4 border rounded-1 p-2">
+    <div class="row justify-content-center mx-0 my-5 py-5">
+        <div class="col-md-4 border rounded-1 px-0">
+            <h5 class="bg-main-light text-center p-2 rounded-top-1">ورود به تک آب صنعت ارم</h5>
             <x-auth-session-status class="mb-4" :status="session('status')" />
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('login') }}" class="p-2">
                 @csrf
-                <div class="mb-3">
-                    <label for="mobile" class="form-label">{{ __('mobile') }}</label>
-                    <input type="text" class="form-control" id="mobile" name="mobile">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="mobile-l">{{ __('mobile') }}</span>
+                    <input type="text" class="form-control" aria-label="mobile-l" aria-describedby="{{ __('mobile') }}" name="mobile">
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">{{ __('password') }}</label>
-                    <input type="password" class="form-control" id="password" name="password">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="password-l">{{ __('password') }}</span>
+                    <input type="password" class="form-control" aria-label="password-l" aria-describedby="{{ __('password') }}" name="password">
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="remember_me" name="remember">
-                    <label class="form-check-label" for="remember">{{ __('Remember me') }}</label>
+                <div class="input-group mb-3">
+                    <div class="input-group-text border rounded me-1">
+                        <input class="form-check-input mt-0" type="checkbox" id="remember_me" name="remember" value="1" aria-label="Checkbox for following text input">
+                    </div>
+                    <label class="form-check-label form-control border-0" for="remember">{{ __('Remember me') }}</label>
                 </div>
-                <button type="submit" class="btn btn-primary">
-                    {{ __('login') }}</button>
+                <div class="d-flex">
+                    <a href="{{route('register')}}" class="btn btn-link me-auto text-decoration-none text-primary text-small">قبلا ثبت نام نکرده ام!</a>
+                    <button type="submit" class="btn btn-success ms-auto">
+                        {{ __('login') }}</button>
+                </div>
+
             </form>
         </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,31 @@
 <x-main-layout>
-    <div class="row justify-content-center my-5 py-5">
-        <div class="col-md-4 border rounded-1 p-2">
-            <form method="POST" action="{{ route('register') }}">
+    <div class="row justify-content-center mx-0 my-5 py-5">
+        <div class="col-md-4 border rounded-1 px-0">
+            <h5 class="bg-main-light text-center p-2 rounded-top-1">ثبت نام در تک آب صنعت ارم</h5>
+            <form method="POST" action="{{ route('register') }}" class="p-2">
                 @csrf
-                <div class="mb-3">
-                    <label for="name" class="form-label">{{ __('nsme') }}</label>
-                    <input type="text" class="form-control" id="name" name="name">
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="name-l">{{ __('name') }}</span>
+                    <input type="text" class="form-control" aria-label="name-l" aria-describedby="{{ __('name') }}" name="name">
                 </div>
-                <div class="mb-3">
-                    <label for="mobile" class="form-label">{{ __('mobile') }}</label>
-                    <input type="text" class="form-control" id="mobile" name="mobile">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="mobile-l">{{ __('mobile') }}</span>
+                    <input type="text" class="form-control" aria-label="mobile-l" aria-describedby="{{ __('mobile') }}" name="mobile">
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">{{ __('password') }}</label>
-                    <input type="password" class="form-control" id="password" name="password">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="password-l">{{ __('password') }}</span>
+                    <input type="password" class="form-control" aria-label="password-l" aria-describedby="{{ __('password') }}" name="password">
                 </div>
-                <div class="mb-3">
-                    <label for="password_confirmation" class="form-label">{{ __('confirmPassword') }}</label>
-                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="password-confirmation-l">{{ __('confirmPassword') }}</span>
+                    <input type="password" class="form-control" aria-label="password-confirmation-l" aria-describedby="{{ __('confirmPassword') }}" name="password_confirmation">
                 </div>
-                <button type="submit" class="btn btn-primary">
-                    {{ __('register') }}</button>
+                <div class="d-flex">
+                    <a href="{{route('login')}}" class="btn btn-link me-auto text-decoration-none text-primary text-small">قبلا ثبت نام کرده ام...</a>
+                    <button type="submit" class="btn btn-success ms-auto">
+                        {{ __('register') }}</button>
+                </div>
             </form>
         </div>
 
