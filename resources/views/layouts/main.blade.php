3ef56c829304456baec44f252f6356a3545b9caa
ahmad reza jalali <hamed.classic.2te@gmail.com>
2026-02-04 12:31:47 +0330

    mobile menu added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,14 +40,11 @@
     </head>
     <body>
     <div class="header">
-        <nav class="navbar navbar-expand-lg pb-0 bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg pb-0 bg-body-tertiary d-none d-md-block">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{route('main.index')}}">
                     <img src="{{asset("img/logo.png")}}" alt="takab logo" width="100">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 ms-3 mb-lg-0">
                         <li class="nav-item px-2">
@@ -190,13 +187,111 @@
                 </div>
             </div>
         </nav>
+
+        <div class="d-md-none row mx-0 py-2 border-bottom border-dark">
+            <div class="col-2 px-1">
+                <a class="navbar-brand text-center" href="{{route('main.index')}}">
+                    <img src="{{asset("img/logo.png")}}" class="pt-2" alt="takab logo" style="width: 100%;">
+                </a>
+            </div>
+            <div class="col-6 px-0">
+                <form action="{{ route('search') }}" method="GET" class="d-flex w-100 me-auto" role="search" >
+                    <input class="form-control rounded-end-0 shadow-none" type="text" placeholder="{{ __('app.product_search') }}"  id="title" name="q">
+                    <button type="submit" class="btn btn-outline-dark rounded-start-0">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="col-1 px-0">
+                <div class="nav-item px-2 text-sm text-center d-flex align-items-center th-1 dropdown">
+                    <button type="button"
+                            class="border-0 pt-3 text-center bg-transparent shadow-none text-sm text-darkgray nav-link p-0"
+                            data-bs-toggle="dropdown">
+                        {{ strtoupper(app()->getLocale()) }}
+                    </button>
+
+                    <ul class="dropdown-menu text-sm text-decoration-none pb-1">
+                        <li><a class="dropdown-item" href="{{ route('lang.switch', 'fa') }}">فارسی</a></li>
+                        <li><a class="dropdown-item" href="{{ route('lang.switch', 'en') }}">English</a></li>
+                        <li><a class="dropdown-item" href="{{ route('lang.switch', 'ar') }}">العربی</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3 px-0">
+                @if(!Auth::check())
+                    <li class="nav-item px-2 border-start text-sm text-center d-block th-1">
+                        <a class="nav-link" href="{{route('login')}}">
+                            <i class="bi bi-person icon-size-2x"></i>
+                            <div>{{ __('app.profile') }}</div>
+                        </a>
+                    </li>
+
+                @else
+                    <li class="nav-item px-2 border-start text-sm text-center d-block th-1 border-left-light-gray dropdown">
+                        <button type="button"
+                                class="border-0 bg-transparent shadow-none text-sm text-darkgray nav-link"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person icon-size-2x"></i>
+                            <div>
+                                    <span class="dropdown-toggle">
+                                {{ Auth::user()->name }}
+                            </div>
+                        </button>
+                        <ul class="dropdown-menu text-sm text-decoration-none pb-1">
+                            <li>
+                                <a class="text-dark text-decoration-none px-2 pb-1 align-self-center d-flex" href="{{route('profile.index')}}">
+                                    <i class="bi bi-house me-2"></i>
+                                    <span>{{ __('app.profile') }}</span>
+                                </a>
+                            </li>
+                            @if(Auth::user()->hasRole(['admin','seller']))
+                                <li>
+                                    <a class="text-dark text-decoration-none px-2 pb-1 align-self-center d-flex" href="{{route('assembly.index')}}">
+                                        <i class="bi bi-bag-check me-2"></i>
+                                        <span>{{ __('app.Assembly') }}</span>
+                                    </a>
+                                </li>
+                            @endif
+                            <li>
+                                <a class="text-dark text-decoration-none px-2 pb-1 align-self-center d-flex" href="{{route('profile.orders.index')}}">
+                                    <i class="bi bi-bag-check me-2"></i>
+                                    <span>{{ __('app.orders') }}</span>
+                                </a>
+                            </li>
+                            @if(!Auth::user()->hasRole(['user']))
+                                <li>
+                                    <a class="text-dark text-decoration-none px-2 pb-1 align-self-center d-flex" href="{{route('admin.index')}}">
+                                        <i class="bi bi-bag-check me-2"></i>
+                                        <span>{{ __('app.admin_panel') }}</span>
+                                    </a>
+                                </li>
+                            @endif
+                            <li><hr class="dropdown-divider my-1"></li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <a href="{{route('logout')}}" class="text-dark text-decoration-none px-2 pb-1 align-self-center d-flex"
+                                       onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                        <i class="bi bi-box-arrow-right me-2"></i>
+                                        <span>{{ __('app.logout') }}</span>
+                                    </a>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+
+                @endif
+            </div>
+        </div>
     </div>
 
     <div class="body">
         {{ $slot }}
     </div>
 
-    <div class="footer bg-footer text-light pt-3">
+    <div class="footer bg-footer text-light pt-3 d-none d-md-block">
         <div class="row mx-0">
             <div class="col-md-4 pt-2">
                 {!! $footerAbout->translation?->content !!}
@@ -226,10 +321,97 @@
             CopyRight 2025 <i class="bi bi-c-circle"></i> TakAb Sanat Eram
         </div>
     </div>
-    <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100">
             <main>
 
             </main>
         </div>
+
+
+<div class="d-md-none" style="width: 100%; height: 60px;"></div>
+    <div class="d-block d-md-none mobile-footer fixed-bottom bg-dark border-top border-light">
+        <div class="row my-1 mx-0 text-center">
+            <div class="col-3 p-1 border-end border-light" :class="{'active': activeMenu === 'Home' }">
+                <a href="/" class="text-light text-decoration-none">
+                    <i class="bi bi-house icon-size-1x d-block"></i>
+                    {{ __('app.home') }}
+                </a>
+            </div>
+            <div class="col-3 p-1 border-end border-light" :class="{ 'active': activeMenu === ($user.auth ? 'ProfileDashboard' : 'Login') }">
+                <a class="text-light text-decoration-none" data-bs-toggle="offcanvas" href="#productMenu" role="button" aria-controls="productMenu">
+                    <i class="bi bi-wrench icon-size-1x d-block"></i>
+                    {{ __('app.products') }}
+                </a>
+            </div>
+            <div class="col-3 p-1 border-end border-light" :class="{ 'active': activeMenu === 'Basket' }">
+                <a href="{{route('cart.show')}}" class="position-relative text-light  text-decoration-none">
+                    <i class="bi bi-basket icon-size-1x d-block"></i>
+                    {{ __('app.basket') }}
+                    <span class="position-absolute top-0 badge text-danger rounded-pill bg-light" style="left:-10px;">0</span>
+                </a>
+            </div>
+            <div class="col-3 p-1">
+                <a class="text-light text-decoration-none" data-bs-toggle="offcanvas" href="#moreMenu" role="button" aria-controls="moreMenu">
+                    <i class="bi bi-list icon-size-1x d-block"></i>
+                    منو
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="moreMenu" aria-labelledby="moreMenuLabel" data-bs-scroll="true" data-bs-backdrop="true">
+
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="moreMenuLabel"></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body p-0 px-2 position-relative">
+            <div class="list-group">
+                <a href="{{route('page.show',['slug'=>'about'])}}" class="list-group-item list-group-item-action border-0 my-1 px-2 py-1">
+                    <i class="bi bi-info-circle icon-size-0x me-1 text-main"></i>
+                    <span>{{ __('app.about') }}</span>
+                </a>
+            </div>
+            <div class="list-group">
+                <a href="{{route('page.contact')}}" class="list-group-item list-group-item-action border-0 my-1 px-2 py-1">
+                    <i class="bi bi-info-circle icon-size-0x me-1 text-main"></i>
+                    <span>{{ __('app.contact') }}</span>
+                </a>
+            </div>
+        </div>
+        <div class="offcanvas-footer">
+            <div class="row mx-0 mt-3 border-top pt-3 justify-content-center certs w-100 mb-3">
+                <div class="col-3 px-1">
+                    <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=675755&Code=L6eJNXqWcqBksxFxbopXAjVnurHzkWsw'>
+                        <img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=675755&Code=L6eJNXqWcqBksxFxbopXAjVnurHzkWsw' alt="enamad" width="100" style='cursor:pointer' code='L6eJNXqWcqBksxFxbopXAjVnurHzkWsw'>
+                    </a>
+                </div>
+            </div>
+            <div class="copyright py-1 font-en-us text-center">
+                Copyright <i class="bi bi-c-circle"></i> 2025
+                <a href="/" class="text-decoration-none">TakAb Sanat Eram</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="productMenu" aria-labelledby="productMenuLabel" data-bs-scroll="true" data-bs-backdrop="true">
+
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="productMenuLabel">لیست محصولات</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body p-0 px-2 position-relative">
+            <div class="list-group">
+
+                @foreach($menuCategories as $cat)
+                    <a href="{{ route('search', ['category'=>$cat->id]) }}" class="list-group-item list-group-item-action border-0 my-1 px-2 py-1">
+                        <i class="bi bi-circle-fill me-1 text-main"></i>
+                        {{ $cat->translation->title }}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
     </body>
 </html>
