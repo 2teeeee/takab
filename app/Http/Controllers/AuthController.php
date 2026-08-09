ab0c36d6e9ee9fde11ac91333f7f75939167afcf
EliasDev <EliasSteinerWork@hotmail.com>
2026-08-09 09:15:07 +0200

    redirect to index for login issues

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,7 +60,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
-            return redirect()->intended(route('main.index'));
+            return redirect(route('admin.index', absolute: false));
         }
 
         return back()->withErrors([
