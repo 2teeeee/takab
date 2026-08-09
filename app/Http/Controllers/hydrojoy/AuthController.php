adfa4e06d672f02d339109313ddd029b92c918a3
EliasDev <EliasSteinerWork@hotmail.com>
2026-08-09 10:58:07 +0200

    link to index issues

--- app/Http/Controllers/hydrojoy/AuthController.php
+++ app/Http/Controllers/hydrojoy/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect(route('admin.index', absolute: false));
+            return redirect()->route('main.index');
         }
 
         return back()->withErrors([
